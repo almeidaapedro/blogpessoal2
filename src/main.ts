80454d5de6eedbc67e4b3790a0b0976866d6d030
Pedro Almeida <almeida.pedroaugusto25@gmail.com>
2024-10-21 11:18:43 -0300

    fix: atualizando main

--- src/main.ts
+++ src/main.ts
@@ -2,24 +2,30 @@ import { ValidationPipe } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
 import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 import { AppModule } from "./app.module";
-
+import * as cors from 'cors'; // Adicione esta importação
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const config = new DocumentBuilder()
-  .setTitle('Sport Map')
-  .setDescription('Projeto de Software do curso')
-  .setVersion('1.0')
-  .addBearerAuth()
-  .build();
+    .setTitle('Sport Map')
+    .setDescription('Projeto de Software do curso')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('/swagger', app, document);
 
   process.env.TZ = '-03:00';
 
   app.useGlobalPipes(new ValidationPipe());
-  app.enableCors();
+  
+  // Habilita CORS
+  app.use(cors({
+    origin: 'http://localhost:5173', // O domínio do seu frontend
+    credentials: true, // Permite cookies, se necessário
+  }));
+
   await app.listen(process.env.PORT || 4000);
 }
 bootstrap();
