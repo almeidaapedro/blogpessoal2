06587354117d0af902cacf649c8e44f79e2539c8
Pedro Almeida <almeida.pedroaugusto25@gmail.com>
2024-10-21 10:28:00 -0300

    fix: usando a classe usuário para outro projeto

--- src/usuario/entities/usuario.entity.ts
+++ src/usuario/entities/usuario.entity.ts
@@ -1,7 +1,7 @@
 import { ApiProperty } from "@nestjs/swagger"
 import { IsNotEmpty, IsEmail, MinLength } from "class-validator"
 import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from "typeorm"
-import { Postagem } from "../../postagem/entities/postagem.entity"
+
 
 
 @Entity({name: "tb_usuarios"})
@@ -19,20 +19,12 @@ export class Usuario {
     @IsEmail()
     @Column({length: 255, nullable: false })
     @ApiProperty({example: "email@email.com.br"}) 
-    public usuario: string
+    public email: string
 
     @IsNotEmpty()
     @MinLength(8)
     @Column({length: 255, nullable: false }) 
     @ApiProperty() 
     public senha: string
-
-    @Column({length: 5000 }) 
-    @ApiProperty() 
-    public foto: string
-
-    @ApiProperty() 
-    @OneToMany(() => Postagem, (postagem) => postagem.usuario)
-    postagem: Postagem[]
 
 }
